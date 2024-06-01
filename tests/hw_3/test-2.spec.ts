5b7fb01056345c7820a0d7ac365b08066c9ff72a
JuliaVo <97233820+JuliyaVo@users.noreply.github.com>
2024-06-01 12:54:30 +0300

    Delete tests/hw_3 directory

--- tests/hw_3/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('DocSearch-Modal has placeholder', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page.getByLabel('Search')).toBeVisible();
-  await page.getByLabel('Search').click();
-  await expect(page.getByPlaceholder('Search docs')).toBeVisible();
-});
